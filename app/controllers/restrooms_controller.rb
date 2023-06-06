ec3535b590616012a079aa33bb1626d97cf3279b
Andreas Aichinger <aichingerandreas@hotmail.com>
2023-06-06 17:59:18 +0200

    create map but not ready yet

--- app/controllers/restrooms_controller.rb
+++ app/controllers/restrooms_controller.rb
@@ -1,41 +1,47 @@
 class RestroomsController < ApplicationController
-    before_action :set_restroom, only: [:show, :destroy]
-    before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_restroom, only: [:show, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-    def index
-        @restrooms = policy_scope(Restroom)
-    end
+  def index
+      @restrooms = policy_scope(Restroom)
+      @markers = @flats.geocoded.map do |flat|
+        {
+          lat: flat.latitude,
+          lng: flat.longitude
+        }
+      end
+  end
 
-    def show
-        authorize @restroom
-    end
+  def show
+      authorize @restroom
+  end
 
-    def new
-        @restroom = Restroom.new
-        authorize @restroom
-    end
-    
-    def create
-        @restroom = Restroom.new(restroom_params)
-        @restroom.user = current_user
-        authorize @restroom 
-    end
-    
-    def destroy
-        @restroom.destroy
-        authorize @restroom
-        redirect_to restrooms_path
-        # redirect to admin dashboard (in future)
-        # notice/alert that the restroom was deleted
-    end
+  def new
+      @restroom = Restroom.new
+      authorize @restroom
+  end
 
-    private
+  def create
+      @restroom = Restroom.new(restroom_params)
+      @restroom.user = current_user
+      authorize @restroom
+  end
 
-    def set_restroom
-        @restroom = Restroom.find(params[:id])
-    end
+  def destroy
+      @restroom.destroy
+      authorize @restroom
+      redirect_to restrooms_path
+      # redirect to admin dashboard (in future)
+      # notice/alert that the restroom was deleted
+  end
 
-    def restroom_params
-        params.require(:restroom).permit(:name, :address, :accessibility, :hygiene_products, :baby_friendly, :pricing, :cleanliness, :photo)
-    end
+  private
+
+  def set_restroom
+      @restroom = Restroom.find(params[:id])
+  end
+
+  def restroom_params
+      params.require(:restroom).permit(:name, :address, :accessibility, :hygiene_products, :baby_friendly, :pricing, :cleanliness, :photo)
+  end
 end
