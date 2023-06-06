6435566eecff841764c2217718df8fa4b97240d1
Andreas Aichinger <aichingerandreas@hotmail.com>
2023-06-06 15:46:15 +0200

    create reviews model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_104850) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_131808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_104850) do
     t.index ["user_id"], name: "index_restrooms_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
+    t.bigint "user_id", null: false
+    t.bigint "restroom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restroom_id"], name: "index_reviews_on_restroom_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +58,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_104850) do
   end
 
   add_foreign_key "restrooms", "users"
+  add_foreign_key "reviews", "restrooms"
+  add_foreign_key "reviews", "users"
 end
