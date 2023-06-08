27b2b24f37288670d006da6e158fe5478ea6c18c
Andreas Aichinger <aichingerandreas@hotmail.com>
2023-06-08 17:39:57 +0200

    fix

--- app/controllers/restrooms_controller.rb
+++ app/controllers/restrooms_controller.rb
@@ -10,7 +10,7 @@ class RestroomsController < ApplicationController
       lat: restroom.latitude,
       lng: restroom.longitude,
       info_window_html: render_to_string(partial: "info_window", locals: {restroom: restroom}),
-      marker_html: render_to_string(partial: "marker")
+      marker_html: render_to_string(partial: "marker", locals: { restroomLngLat: [restroom.longitude, restroom.latitude] })
     }
     end
   end
@@ -80,4 +80,3 @@ class RestroomsController < ApplicationController
     params.require(:restroom).permit(:name, :address, :accessibility, :hygiene_products, :baby_friendly, :pricing, :cleanliness, :photo)
   end
 end
-
