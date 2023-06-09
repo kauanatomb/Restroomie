476867bf12d98faaa781de760469ba6e213c3d39
kauana tombolato <kauanatomb@gmail.com>
2023-06-09 10:08:18 +0200

    fix index page with style

--- app/models/restroom.rb
+++ app/models/restroom.rb
@@ -3,6 +3,6 @@ class Restroom < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many :bookmarks, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many_attached :photos
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   belongs_to :restroom, dependent: :destroy
   validates :rating, presence: true, inclusion: { in: 0..5 }
 end
