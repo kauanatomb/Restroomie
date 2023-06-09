596535d5a339f9a919cf3e86cb0b8a38fa939e34
Yaya Rezai <yayarezai3@gmail.com>
2023-06-09 11:50:14 +0200

    seeds have been corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_many :restrooms
   has_many :bookmarks, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   def admin?
     return admin
   end
