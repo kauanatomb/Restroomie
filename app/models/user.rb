c7295fc708cb3314c0b78559d78b4abe2111d232
kauana tombolato <kauanatomb@gmail.com>
2023-06-09 11:11:44 +0200

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :restrooms, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
