50d1d07d8bb768b6d986182d354a8a0d41ccbb84
Andreas Aichinger <aichingerandreas@hotmail.com>
2023-06-13 11:18:19 +0200

    map

--- app/javascript/controllers/slide_controller.js
+++ app/javascript/controllers/slide_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="slide"
 export default class extends Controller {
-  static targets = ["partialContainer"]
+  static targets = ["partialContainer", "routeButton"]
 
   replace(event) {
+    this.coordinates = event.target.dataset.restroomlnglat
     fetch(event.currentTarget.dataset.url, {
       method: 'GET',
       headers: { 'Accept': 'text/plain' }
@@ -12,8 +13,10 @@ export default class extends Controller {
       .then(response => response.text())
       .then((data) => {
         this.partialContainerTarget.innerHTML = data
+        this.routeButtonTarget.dataset.coordinates = this.coordinates
       })
   }
+
 
   slideUp() {
     // Animation code goes here
