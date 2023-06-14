d8e9c3b6b35eb27f14ce2a18302bfbf6c81ff67e
Patrick Bird <p_h_bird@yahoo.fr>
2023-06-14 16:06:09 +0200

    Redid seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,288 +15,131 @@ User.create!(
   username: "User 2"
 )
 
-User.create!(
-  email: "user3@example.com",
-  password: "123456",
-  username: "User 3"
-)
-
-Restroom.create!(
-  name: "Alexanderplatz",
-  address: "Alexanderplatz 1, 10178 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: false,
-  pricing: 2.50,
-  # cleanliness: true,
-  longitude: 13.4133,
-  latitude: 52.5217,
-  user_id: User.first.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Friedrichstraße",
-  address: "Friedrichstraße 123, 10117 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: true,
-  pricing: 3.00,
-  # cleanliness: true,
-  longitude: 13.3889,
-  latitude: 52.5200,
-  user_id: User.second.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Potsdamer Platz",
-  address: "Potsdamer Platz 2, 10785 Berlin",
-  accessibility: false,
-  # hygiene_products: false,
-  baby_friendly: false,
-  pricing: 1.50,
-  # cleanliness: false,
-  longitude: 13.3736,
-  latitude: 52.5096,
-  user_id: User.first.id,
-  approved: false
-)
-
-Restroom.create!(
-  name: "Unter den Linden",
-  address: "Unter den Linden 77, 10117 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: true,
-  pricing: 2.00,
-  # cleanliness: true,
-  longitude: 13.3944,
-  latitude: 52.5163,
-  user_id: User.third.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Kurfürstendamm",
-  address: "Kurfürstendamm 220, 10719 Berlin",
-  accessibility: true,
-  # hygiene_products: false,
-  baby_friendly: true,
-  pricing: 2.50,
-  # cleanliness: true,
-  longitude: 13.3307,
-  latitude: 52.5039,
-  user_id: User.second.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Schlossplatz",
-  address: "Schlossplatz 5, 10178 Berlin",
-  accessibility: false,
-  # hygiene_products: true,
-  baby_friendly: false,
-  pricing: 1.00,
-  # cleanliness: false,
-  longitude: 13.4023,
-  latitude: 52.5173,
-  user_id: User.first.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Brandenburger Tor",
-  address: "Brandenburger Tor 1, 10117 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: true,
-  pricing: 2.00,
-  # cleanliness: true,
-  longitude: 13.3777,
-  latitude: 52.5163,
-  user_id: User.third.id,
-  approved: false
-)
-
-Restroom.create!(
-  name: "Museum Island",
-  address: "Museum Island, 10178 Berlin",
-  accessibility: true,
-  # hygiene_products: false,
-  baby_friendly: false,
-  pricing: 1.50,
-  # cleanliness: true,
-  longitude: 13.3986,
-  latitude: 52.5199,
-  user_id: User.second.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Gendarmenmarkt",
-  address: "Gendarmenmarkt, 10117 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: true,
-  pricing: 3.00,
-  # cleanliness: true,
-  longitude: 13.3928,
-  latitude: 52.5139,
-  user_id: User.first.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Hackescher Markt",
-  address: "Hackescher Markt, 10178 Berlin",
-  accessibility: false,
-  # hygiene_products: true,
-  baby_friendly: false,
-  pricing: 1.50,
-  # cleanliness: false,
-  longitude: 13.4022,
-  latitude: 52.5225,
-  user_id: User.third.id,
-  approved: false
-)
-
-
-Restroom.create!(
-  name: "Tiergarten Park",
-  address: "Straße des 17. Juni, 10557 Berlin",
-  accessibility: true,
-  # hygiene_products: false,
-  baby_friendly: true,
-  pricing: 2.00,
-  # cleanliness: true,
-  longitude: 13.3504,
-  latitude: 52.5143,
-  user_id: User.first.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "East Side Gallery",
-  address: "Mühlenstraße 3-100, 10243 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: false,
-  pricing: 1.50,
-  # cleanliness: false,
-  longitude: 13.4415,
-  latitude: 52.5054,
-  user_id: User.third.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Charlottenburg Palace",
-  address: "Spandauer Damm 10-22, 14059 Berlin",
-  accessibility: false,
-  # hygiene_products: true,
-  baby_friendly: false,
-  pricing: 1.00,
-  # cleanliness: true,
-  longitude: 13.2957,
-  latitude: 52.5209,
-  user_id: User.second.id,
-  approved: false
-)
-
-Restroom.create!(
-  name: "Berlin Wall Memorial",
-  address: "Bernauer Straße 119, 13355 Berlin",
-  accessibility: true,
-  # hygiene_products: false,
-  baby_friendly: true,
-  pricing: 2.00,
-  # cleanliness: true,
-  longitude: 13.3934,
-  latitude: 52.5387,
-  user_id: User.first.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Berlin Cathedral",
-  address: "Am Lustgarten, 10178 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: true,
-  pricing: 2.50,
-  # cleanliness: true,
-  longitude: 13.4019,
-  latitude: 52.5186,
-  user_id: User.third.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Olympic Stadium",
-  address: "Olympischer Platz 3, 14053 Berlin",
-  accessibility: false,
-  # hygiene_products: true,
-  baby_friendly: false,
-  pricing: 1.50,
-  # cleanliness: false,
-  longitude: 13.2417,
-  latitude: 52.5121,
-  user_id: User.second.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Victory Column",
-  address: "Großer Stern, 10557 Berlin",
-  accessibility: true,
-  # hygiene_products: true,
-  baby_friendly: true,
-  pricing: 3.00,
-  # cleanliness: true,
-  longitude: 13.3427,
-  latitude: 52.5146,
-  user_id: User.first.id,
-  approved: false
-)
-
-Restroom.create!(
-  name: "Berlin TV Tower",
-  address: "Panoramastraße 1A, 10178 Berlin",
-  accessibility: true,
-  # hygiene_products: false,
-  baby_friendly: false,
-  pricing: 2.00,
-  # cleanliness: true,
-  longitude: 13.4094,
-  latitude: 52.5208,
-  user_id: User.third.id,
-  approved: true
-)
-
-Restroom.create!(
-  name: "Berlin Central Station",
+restroom1 = Restroom.create!(
+  name: "Haupti",
   address: "Europaplatz 1, 10557 Berlin",
   accessibility: true,
-  # hygiene_products: true,
   baby_friendly: true,
-  pricing: 2.50,
-  # cleanliness: true,
+  pricing: 0.50,
   longitude: 13.3695,
   latitude: 52.5250,
   user_id: User.second.id,
   approved: true
 )
 
-Restroom.all.each do |restroom|
-  1.times do
-    random_file = URI.open(Faker::LoremFlickr.image(size: "300x250", search_terms: ['restroom']))
-    restroom.photos.attach(io: random_file, filename: "#{restroom.name.parameterize}.png", content_type: "image/png")
-    restroom.save!
-  end
+file1 = URI.open("https://images.unsplash.com/photo-1414452110837-9dab484a417d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80")
+restroom1.photos.attach(io: file1, filename: "#{restroom1.name.parameterize}.png", content_type: "image/png")
 
+# https://images.unsplash.com/photo-1626039822277-0f05380ca588?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80
+
+restroom1.save!
+
+restroom2 = Restroom.create!(
+  name: "Friedi",
+  address: "Friedrichstraße 123, 10117 Berlin",
+  accessibility: true,
+  baby_friendly: true,
+  pricing: 0.50,
+  longitude: 13.3889,
+  latitude: 52.5200,
+  user_id: User.second.id,
+  approved: true
+)
+
+file2 = URI.open("https://images.unsplash.com/photo-1592083646587-2c84f8e0fd2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80")
+restroom2.photos.attach(io: file2, filename: "#{restroom2.name.parameterize}.png", content_type: "image/png")
+
+restroom2.save!
+
+restroom3 = Restroom.create!(
+  name: "Burgi",
+  address: "Potsdamer Platz 2, 10785 Berlin",
+  accessibility: false,
+  baby_friendly: false,
+  pricing: nil,
+  longitude: 13.3736,
+  latitude: 52.5096,
+  user_id: User.first.id,
+  approved: false
+)
+
+file3 = URI.open("https://images.unsplash.com/photo-1609369069815-5b715eb143b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")
+restroom3.photos.attach(io: file3, filename: "#{restroom3.name.parameterize}.png", content_type: "image/png")
+
+restroom3.save!
+
+restroom4 = Restroom.create!(
+  name: "Brandi",
+  address: "Unter den Linden 77, 10117 Berlin",
+  accessibility: true,
+  baby_friendly: true,
+  pricing: nil,
+  longitude: 13.3944,
+  latitude: 52.5163,
+  user_id: User.second.id,
+  approved: true
+)
+
+file4 = URI.open("https://plus.unsplash.com/premium_photo-1676320514140-13d16e487b02?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80")
+restroom4.photos.attach(io: file4, filename: "#{restroom4.name.parameterize}.png", content_type: "image/png")
+
+restroom4.save!
+
+
+restroom5 = Restroom.create!(
+  name: "Alex",
+  address: "Panoramastraße 1A, 10178 Berlin",
+  accessibility: true,
+  baby_friendly: true,
+  pricing: 0.50,
+  longitude: 13.3695,
+  latitude: 52.5250,
+  user_id: User.second.id,
+  approved: true
+)
+
+file5 = URI.open("https://empire-s3-production.bobvila.com/articles/wp-content/uploads/2022/03/Where-to-put-litter-box_1.jpg")
+restroom5.photos.attach(io: file5, filename: "#{restroom5.name.parameterize}.png", content_type: "image/png")
+
+restroom5.save
+
+restroom6 = Restroom.create!(
+  name: "Tim Raui",
+  address: "Rudi-Dutschke-Straße 26, 10969 Berlin",
+  accessibility: true,
+  baby_friendly: false,
+  pricing: nil,
+  longitude: 13.4094,
+  latitude: 52.5208,
+  user_id: User.second.id,
+  approved: true
+)
+
+file6 = URI.open("https://images.unsplash.com/photo-1632681179698-35ac572f6510?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80")
+restroom6.photos.attach(io: file6, filename: "#{restroom6.name.parameterize}.png", content_type: "image/png")
+
+restroom6.save!
+
+restroom7 = Restroom.create!(
+  name: "Bessi",
+  address: "Besselstraße, 10969 Berlin",
+  accessibility: true,
+  baby_friendly: false,
+  pricing: nil,
+  longitude: 13.4094,
+  latitude: 52.5208,
+  user_id: User.first.id,
+  approved: true
+)
+
+file7 = URI.open("https://images.unsplash.com/photo-1465257474426-bd133d4db35e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=80")
+restroom7.photos.attach(io: file7, filename: "#{restroom7.name.parameterize}.png", content_type: "image/png")
+
+restroom7.save!
+
+Restroom.all.each do |restroom|
   rand(3..4).times do
-    comment = Faker::Lorem.paragraph[0..39]
+    comment = Faker::Lorem.paragraph[0..19]
     Review.create!(
       rating: rand(1..5),
       comment: comment,
